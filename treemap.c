45d8d0766f3175251b39edede1903b9be22b6f3a
vicentesarabia <>
2022-04-24 01:29:12 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -101,13 +101,13 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
     {
       return aux->pair->value;
     }
-    if(tree->lower_than(key,aux->pair->key)==0)
-    {
-      aux=aux->right;
-    }
-    else if(tree->lower_than(key,aux->pair->key)==1)
+    if(tree->lower_than(key,aux->pair->key)==1)
     {
       aux=aux->left;
+    }
+    else if(tree->lower_than(key,aux->pair->key)==0)
+    {
+      aux=aux->right;
     }
   }
   
