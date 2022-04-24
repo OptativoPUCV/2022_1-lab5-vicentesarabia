dce8a798f1640f37e9f04caef855b27882ec8682
vicentesarabia <>
2022-04-24 18:03:40 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -151,7 +151,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
-  TreeNode aux = tree->root;
+  TreeNode *aux = tree->root;
   while(aux->left!=NULL)
     {
       if(aux->left!=NULL)
